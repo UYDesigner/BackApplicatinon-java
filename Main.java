b9a26bd97fd36d4c8e0014cd7332539eb8c5d566
Urvashi Yadav <156706321+UYDesigner@users.noreply.github.com>
2024-08-14 16:00:22 +0530

    Java file
    Java file

---
+++ Main.java
@@ -0,0 +1,15 @@
+import Views.WelcomeView;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        WelcomeView w = new WelcomeView();
+
+        do{
+            w.welcomeScreen();
+        }while (true);
+
+    }
+
+}
